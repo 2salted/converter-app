9b6eedf39184124113620c90c1a0f495c8dbdc39
2salted <noamgraf123@gmail.com>
2024-03-11 09:30:41 -0100

    stable update added calc.tsx, update before big update

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Homepage from "./pages/Homepage";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/calculator/:calcId",
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Homepage />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
