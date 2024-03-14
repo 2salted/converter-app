e6d22a00a160c69ad92c5e5f0294a6bad5ac6517
2salted <noamgraf123@gmail.com>
2024-03-14 12:41:41 -0100

    added inputs for each object using map and added some array objects to the searches.ts

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -1,13 +1,43 @@
+import { useParams } from "react-router-dom";
+import { calculators } from "../searches";
+
 export default function Calculator() {
+  const { calcId } = useParams();
+  let foundSelected = calculators.find((a) => a.queryId === calcId);
+
   return (
     <div
-      className="flex justify-center h-screen box-border text-white font-jetbrainsmono"
+      className="flex justify-center h-screen box-border font-jetbrainsmono"
       style={{
         backgroundColor: "#0093E9",
         backgroundImage: "linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)",
       }}
     >
-      <input></input>
+      <div className="flex items-center text-black">
+        <div className="pr-10">
+          {foundSelected &&
+            foundSelected?.inputs.map((input, index) => {
+              return (
+                <div className="flex flex-row" key={index}>
+                  <div>{input.name}</div>
+                  <input
+                    className="p-2 border border-gray-300 rounded-l-md outline-none rounded-20"
+                    type="text"
+                    placeholder="Input 1"
+                  />
+                </div>
+              );
+            })}
+        </div>
+        <div className="px-3 text-7xl text-white">=</div>
+        <div className="pl-10">
+          <input
+            className="p-2 border border-gray-300 rounded-r-md outline-none rounded-20"
+            type="text"
+            placeholder="Input 2"
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/searches.ts
+++ src/searches.ts
@@ -6,9 +6,30 @@ export let calculators = [
     inputs: [{ name: "celsius", label: "C" }],
     outputs: [{ name: "fahrenheit", label: "F" }],
   },
-  { queryId: "fahrenheit", topic: "Conversion", query: "Fahrenheit Converter" },
-  { queryId: "bmicalculator", topic: "Health", query: "BMI Calculator" },
-  { queryId: "deltav", topic: "Physics", query: "Delta V Calculator" },
+  {
+    queryId: "fahrenheit",
+    topic: "Conversion",
+    query: "Fahrenheit Converter",
+    inputs: [{ name: "fahrenheit", label: "F" }],
+    outputs: [{ name: "celsius", label: "F" }],
+  },
+  {
+    queryId: "bmicalculator",
+    topic: "Health",
+    query: "BMI Calculator",
+    inputs: [
+      { name: "height", label: "H" },
+      { name: "weight", label: "W" },
+    ],
+    outputs: [{ name: "bmi", label: "bmi" }],
+  },
+  {
+    queryId: "deltav",
+    topic: "Physics",
+    query: "Delta V Calculator",
+    inputs: [{ name: "no name", label: "nn" }],
+    outputs: [{ name: "no name", label: "nn" }],
+  },
 ];
 
 function celciusToFahrenheit(inputs: number[]): number[] {
@@ -19,15 +40,10 @@ function fahrenheitToCelsius(inputs: number[]): number[] {
   return [(inputs[0] - 32) * (5 / 9)];
 }
 
-function gravityCalculator(inputs: number[]): number[] {
-  return [(inputs[0] * inputs[1] * inputs[2]) / inputs[3] ** 2];
-}
-
 export function calculate(inputs: number[], queryId: string): number[] {
   const calculationMap: { [key: string]: (inputs: number[]) => number[] } = {
     celsius: celciusToFahrenheit,
     fahrenheit: fahrenheitToCelsius,
-    gravity: gravityCalculator,
   };
   return calculationMap[queryId](inputs);
 }
